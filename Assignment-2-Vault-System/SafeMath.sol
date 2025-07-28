c8aeaa800bb6a955cd5954303988077dfb108570
AKJ <152957866+Akchive@users.noreply.github.com>
2025-07-29 02:05:35 +0530

    Create SafeMath.sol

---
+++ Assignment-2-Vault-System/SafeMath.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SafeMath {
+    function add(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        require(c >= a, "Addition overflow");
+        return c;
+    }
+    function sub(uint a, uint b) internal pure returns (uint) {
+        require(b <= a, "Subtraction underflow");
+        uint c = a - b;
+        return c;
+    }
+}
