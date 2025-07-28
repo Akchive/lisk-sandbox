85a7b209aa192a920fbee4aa018b21c03462acf7
AKJ <152957866+Akchive@users.noreply.github.com>
2025-07-29 03:05:51 +0530

    Update VulnerablePiggyBank.sol

--- Assignment-3-VulnerablePiggyBank-Audit/VulnerablePiggyBank.sol
+++ Assignment-3-VulnerablePiggyBank-Audit/VulnerablePiggyBank.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+//This is the original contract with vulnerabilities. to show what we are auditing and improving.
 contract VulnerablePiggyBank {
     address public owner;
 
